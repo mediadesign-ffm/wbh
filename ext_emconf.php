7511af94bcc93c4a1f6c4c24f3318761f6ea0a07
Michael Lang <info@mediadesign-ffm.de>
2024-06-20 11:39:46 +0200

    v11

---
+++ ext_emconf.php
@@ -0,0 +1,27 @@
+<?php
+
+/**********************************************************************
+ * Extension Manager/Repository config file for Sitepackage ext "WBH".
+ *********************************************************************/
+
+/** @var string $_EXTKEY */
+$EM_CONF[$_EXTKEY] = [
+    'title'            => 'wbh - Basic Installation',
+    'description'      => 'wbh - Distribution.',
+    'version'          => '1.0.0',
+    'state'            => 'alpha',
+    'category'         => 'templates',
+    'author'           => 'Michael Lang',
+    'author_email'     => 'michael.lang@mediadesign-ffm.de',
+    'author_company'   => 'mediadesign',
+    'uploadfolder'     => false,
+    'createDirs'       => '',
+    'clearCacheOnLoad' => true,
+    'constraints'      => [
+        'depends'   => [
+            'typo3'            => '11.5.0-',
+        ],
+        'conflicts' => [],
+        'suggests'  => [],
+    ],
+];
